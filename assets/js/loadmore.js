729b4f2781c9379bbd608bc03767c202a17669f4
YuliaRudenko <89047334+YuliaRudenko@users.noreply.github.com>
2022-05-21 22:47:08 +0300

    Add files via upload

--- assets/js/loadmore.js
+++ assets/js/loadmore.js
@@ -1,1 +1,22 @@
-
+jQuery(function($){
+    $('#loadmore').click(function(){ // клик на кнопку
+        $(this).text('Загрузка...'); // меняем текст на кнопке
+        // получаем переменные
+        var data = {
+            'action': 'loadmore',
+            'query': posts_vars,
+        };
+        // отправляем Ajax-запрос 
+        $.ajax({
+            url:ajaxurl,
+            data:data,
+            type:'POST',
+            success:function(data){
+                if(data) { 
+                    $('#loadmore').text('Показать ещё').before(data);
+                    $('#loadmore').remove(); // удаляем кнопку
+                }
+            }
+        });
+    });
+});
